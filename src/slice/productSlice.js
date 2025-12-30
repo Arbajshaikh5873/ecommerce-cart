01989f06b71126bac96b575eeb779df0256f0473
Arbaj Shaikh <sarbaj637@gmail.com>
2025-12-30 11:46:30 +0530

    written Redux logic

--- src/slice/productSlice.js
+++
@@ -1,9 +0,0 @@
-import { createSlice } from "@reduxjs/toolkit";
-
-const ProductSlice = createSlice({
-  name: "product",
-  initialState: [],
-  reducers: {},
-});
-
-export default ProductSlice.reducer;
